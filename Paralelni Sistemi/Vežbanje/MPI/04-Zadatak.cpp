426ce6b7e752183388dfa327854d4fe9cb0a1ab2
sssteeefaaan <stefan.sa.aleksic@gmail.com>
2022-04-29 01:24:14 +0200

    pakao.....

--- Paralelni Sistemi/Vežbanje/MPI/04-Zadatak.cpp
+++ Paralelni Sistemi/Vežbanje/MPI/04-Zadatak.cpp
@@ -1,52 +1,52 @@
-//#include <mpi.h>
-//#include <stdio.h>
-//#include <string>
-//
-//#define N 32
-//
-//struct str {
-//	int count;
-//	char chars[N];
-//};
-//
-//int main(int argc, char** argv)
-//{
-//	int rank, size, b_len[2], types[2];
-//	MPI_Aint disp[2];
-//	MPI_Datatype new_type, mpi_string;
-//	str temp{};
-//
-//	MPI_Init(&argc, &argv);
-//	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//	MPI_Comm_size(MPI_COMM_WORLD, &size);
-//
-//	MPI_Type_contiguous(N, MPI_CHAR, &mpi_string);
-//	MPI_Type_commit(&mpi_string);
-//
-//	types[0] = MPI_INT;
-//	types[1] = mpi_string;
-//
-//	MPI_Get_address(&temp.count, &disp[0]);
-//	MPI_Get_address(&temp.chars, &disp[1]);
-//	disp[1] -= disp[0];
-//	disp[0] = 0;
-//
-//	b_len[0] = b_len[1] = 1;
-//
-//	MPI_Type_create_struct(2, b_len, disp, types, &new_type);
-//	MPI_Type_commit(&new_type);
-//
-//	if (rank == 0)
-//	{
-//		gets_s<N>(temp.chars);
-//		temp.count = strlen(temp.chars);
-//	}
-//
-//	MPI_Bcast(&temp, 1, new_type, 0, MPI_COMM_WORLD);
-//
-//	printf("P[%d]: Hello world! strlen(%s)=%d\n", rank, temp.chars, temp.count);
-//	fflush(stdout);
-//
-//	MPI_Finalize();
-//	return 0;
-//}
+#include <mpi.h>
+#include <stdio.h>
+#include <string>
+
+#define N 32
+
+struct str {
+	int count;
+	char chars[N];
+};
+
+int main(int argc, char** argv)
+{
+	int rank, size, b_len[2], types[2];
+	MPI_Aint disp[2];
+	MPI_Datatype new_type, mpi_string;
+	str temp{};
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	MPI_Type_contiguous(N, MPI_CHAR, &mpi_string);
+	MPI_Type_commit(&mpi_string);
+
+	types[0] = MPI_INT;
+	types[1] = mpi_string;
+
+	MPI_Get_address(&temp.count, &disp[0]);
+	MPI_Get_address(&temp.chars, &disp[1]);
+	disp[1] -= disp[0];
+	disp[0] = 0;
+
+	b_len[0] = b_len[1] = 1;
+
+	MPI_Type_create_struct(2, b_len, disp, types, &new_type);
+	MPI_Type_commit(&new_type);
+
+	if (rank == 0)
+	{
+		gets_s<N>(temp.chars);
+		temp.count = strlen(temp.chars);
+	}
+
+	MPI_Bcast(&temp, 1, new_type, 0, MPI_COMM_WORLD);
+
+	printf("P[%d]: Hello world! strlen(%s)=%d\n", rank, temp.chars, temp.count);
+	fflush(stdout);
+
+	MPI_Finalize();
+	return 0;
+}
--- Paralelni Sistemi/Vežbanje/MPI/Oktobar-2021.cpp
+++ Paralelni Sistemi/Vežbanje/MPI/Oktobar-2021.cpp
@@ -1,162 +1,158 @@
 #include <mpi.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include <iostream>
-#include <sstream>
 using namespace std;
 
-bool check_result(int* res, int* A, int* B, int n, int m, int k);
-void print_matrix(const char* lbl, int* M, int n, int m);
-void print_vector(const char* lbl, int* v, int n);
+bool check_result(int* A, int* b, int* c, int n);
 
 int main(int argc, char** argv)
 {
-	int rank, size;
+	int rank, size, col_rank, row_rank;
+
+	const int n = 1 << 3, p = 1 << 4, q = sqrt(p), s = n / q;
+	MPI_Status stat;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	if (size != p)
+		exit(-100);
 
-	const int n = 1 << 3,
-		p = 1 << 4,
-		q = sqrt(p),
-		s = n / q;
+	MPI_Comm col_comm, row_comm;
+	MPI_Comm_split(MPI_COMM_WORLD, rank % q, rank / q, &col_comm);
+	MPI_Comm_split(MPI_COMM_WORLD, rank / q, rank % q, &row_comm);
 
-	MPI_Comm row_comm, col_comm;
-	MPI_Comm_split(MPI_COMM_WORLD, rank % q, 0, &col_comm);
-	MPI_Comm_split(MPI_COMM_WORLD, rank / q, 0, &row_comm);
-
-	int col_rank, row_rank;
 	MPI_Comm_rank(col_comm, &col_rank);
 	MPI_Comm_rank(row_comm, &row_rank);
 
-	int A[n][n], B[n][n], C[n][n],
-		* local_A, * local_B, * local_C, * local_part_C;
+	int A[n][n], b[n], c[n],
+		* local_A, * local_b, * local_part_c, * local_c;
 
-	local_A = (int*)malloc(sizeof(int) * s * n);
-	local_B = (int*)malloc(sizeof(int) * n * s);
-	local_part_C = (int*)malloc(sizeof(int) * s * s);
-	local_C = (int*)malloc(sizeof(int) * s * n);
+	local_A = (int*)malloc(sizeof(int) * s * s);
+	local_b = (int*)malloc(sizeof(int) * s);
+	local_part_c = (int*)malloc(sizeof(int) * s);
+	local_c = (int*)malloc(sizeof(int) * s);
 
 	if (rank == 0)
 	{
 		for (int i = 0; i < n; i++)
 		{
 			for (int j = 0; j < n; j++)
-			{
 				A[i][j] = i * n + j;
-				B[i][j] = 0;
-			}
-			B[i][n - 1 - i] = 1;
+			b[i] = i;
 		}
-
-		print_matrix("A", &A[0][0], n, n);
-		print_matrix("B", &B[0][0], n, n);
 	}
 
-	MPI_Datatype tmp, nt_col, nt_row;
-	MPI_Type_vector(s, n, n * q, MPI_INT, &tmp);
-	MPI_Type_create_resized(tmp, 0, n * sizeof(int), &nt_row);
-	MPI_Type_commit(&nt_row);
-
-	MPI_Type_vector(s, 1, q, MPI_INT, &tmp);
-	MPI_Type_create_resized(tmp, 0, sizeof(int), &tmp);
-	MPI_Type_vector(n, 1, n, tmp, &tmp);
-	MPI_Type_create_resized(tmp, 0, sizeof(int), &nt_col);
-	MPI_Type_commit(&nt_col);
+	MPI_Datatype nt;
+	MPI_Type_vector(s, 1, q, MPI_INT, &nt);
+	MPI_Type_create_resized(nt, 0, sizeof(int), &nt);
+	MPI_Type_commit(&nt);
 
 	if (col_rank == 0)
-		MPI_Bcast(A, n * n, MPI_INT, 0, row_comm);
+		MPI_Scatter(&b[0], 1, nt, local_b, s, MPI_INT, 0, row_comm);
+
+	MPI_Bcast(local_b, s, MPI_INT, 0, col_comm);
+
+#pragma region a)
+
+	/*MPI_Datatype mnt;
+	MPI_Type_vector(s, 1, n, nt, &mnt);
+	MPI_Type_create_resized(mnt, 0, sizeof(int), &mnt);
+	MPI_Type_commit(&mnt);
+	
+	if (rank == 0)
+	{
+		for (int i = 0; i < s; i++)
+		{
+			for (int j = 0; j < s; j++)
+				local_A[i * s + j] = A[i][j * q];
+		}
+
+		for (int i = 1; i < p; i++)
+			MPI_Send(&A[i / q * s][i % q], 1, mnt, i, 0, MPI_COMM_WORLD);
+	}
+	else
+		MPI_Recv(local_A, s * s, MPI_INT, 0, 0, MPI_COMM_WORLD, &stat);*/
+
+#pragma endregion
+
+#pragma region b)
+
+	MPI_Datatype mnt;
+	MPI_Type_vector(s, n, n, MPI_INT, &mnt);
+	MPI_Type_create_resized(mnt, 0, sizeof(int) * n * s, &mnt);
+	MPI_Type_commit(&mnt);
+
+	int* local_part_A = (int*)malloc(sizeof(int) * n * s);
 	if (row_rank == 0)
-		MPI_Bcast(B, n * n, MPI_INT, 0, col_comm);
+		MPI_Scatter(&A[0][0], 1, mnt, local_part_A, s * n, MPI_INT, 0, col_comm);
 
-	MPI_Scatter(A, 1, nt_row, local_A, s * n, MPI_INT, 0, col_comm);
-	MPI_Scatter(B, 1, nt_col, local_B, n * s, MPI_INT, 0, row_comm);
+	MPI_Type_vector(s, 1, n, nt, &mnt);
+	MPI_Type_create_resized(mnt, 0, sizeof(int), &mnt);
+	MPI_Type_commit(&mnt);
 
+	MPI_Scatter(local_part_A, 1, mnt, local_A, s * s, MPI_INT, 0, row_comm);
+	free(local_part_A);
+
+#pragma endregion
+
+	struct {
+		int min;
+		int rank;
+	}local_min = { INT16_MAX, rank }, global_min{};
 	for (int i = 0; i < s; i++)
 	{
-		for (int j = 0; j < s; j++)
+		local_part_c[i] = 0;
+		for (int k = 0; k < s; k++)
 		{
-			local_part_C[i * s + j] = 0;
-			for (int k = 0; k < n; k++)
-				local_part_C[i * s + j] += local_A[i * n + k] * local_B[k * s + j];
+			local_part_c[i] += local_A[i * s + k] * local_b[k];
+			if (local_min.min > local_A[i * s + k])
+				local_min.min = local_A[i * s + k];
 		}
 	}
 
-	int* strides = (int*)malloc(sizeof(int) * s * s);
-	int* block_lens = (int*)malloc(sizeof(int) * s * s);
+	MPI_Reduce(&local_min, &global_min, 1, MPI_2INT, MPI_MINLOC, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&global_min, 1, MPI_2INT, 0, MPI_COMM_WORLD);
+	MPI_Reduce(local_part_c, local_c, s, MPI_INT, MPI_SUM, global_min.rank % q, row_comm);
 
-	for (int i = 0; i < s; i++)
+	if (global_min.rank % q == row_rank)
 	{
-		for (int j = 0; j < s; j++)
+		MPI_Gather(local_c, s, MPI_INT, c, s, MPI_INT, global_min.rank / q, col_comm);
+		if (rank == global_min.rank)
 		{
-			block_lens[i * s + j] = 1;
-			strides[i * s + j] = i * n + j * q;
+			MPI_Request req;
+			cout << "\n\nP[" << col_rank << ", " << row_rank << "] I have the minimum: " << global_min.min << endl;
+			MPI_Isend(c, n, MPI_INT, 0, 0, MPI_COMM_WORLD, &req);
 		}
 	}
-	MPI_Type_indexed(s * s, block_lens, strides, MPI_INT, &tmp);
-	MPI_Type_create_resized(tmp, 0, sizeof(int), &nt_col);
-	MPI_Type_commit(&nt_col);
 
-	MPI_Gather(local_part_C, s * s, MPI_INT, local_C, 1, nt_col, 0, row_comm);
-
-	if (row_rank == 0)
+	if (rank == 0)
 	{
-		MPI_Gather(local_C, s * n, MPI_INT, C, 1, nt_row, 0, col_comm);
-
-		if (rank == 0)
-		{
-			print_matrix("C", &C[0][0], n, n);
-
-			if (check_result(&C[0][0], &A[0][0], &B[0][0], n, n, n))
-				cout << "\n\nCorrect!" << endl;
-			else
-				cout << "\n\nFalse!" << endl;
-		}
+		MPI_Recv(c, n, MPI_INT, global_min.rank, 0, MPI_COMM_WORLD, &stat);
+		if (check_result(&A[0][0], b, c, n))
+			cout << "\n\nCorrect!" << endl;
+		else
+			cout << "\n\nFalse!" << endl;
 	}
 
-	free(block_lens);
-	free(strides);
-	free(local_part_C);
-	free(local_C);
-	free(local_B);
+	free(local_part_c);
+	free(local_c);
+	free(local_b);
 	free(local_A);
-
+	
 	MPI_Finalize();
 
 	return 0;
 }
 
-bool check_result(int* res, int* A, int* B, int n, int m, int k)
+bool check_result(int* A, int* b, int* c, int n)
 {
-	bool c = true;
-	for (int i = 0; c && i < n; i++)
+	bool ret = true;
+	for (int i = 0; ret&& i < n; i++)
 	{
-		for (int j = 0; c && j < k; j++)
-		{
-			int val = 0;
-			for (int p = 0; p < m; p++)
-				val += A[i * m + p] * B[p * k + j];
-			c = val == res[i * k + j];
-		}
+		int temp = 0;
+		for (int j = 0; ret && j < n; j++)
+			temp += A[i * n + j] * b[j];
+		ret = c[i] == temp;
 	}
-	return c;
-}
-
-void print_matrix(const char* lbl, int* M, int n, int m)
-{
-	cout << "\n\n" << lbl << ":\n";
-	for (int i = 0; i < n; i++)
-	{
-		cout << "|\t";
-		for (int j = 0; j < m; (cout << M[i * m + j++] << "\t"));
-		cout << "|\n";
-	}
-}
-void print_vector(const char* lbl, int* v, int n)
-{
-	cout << "\n\n" << lbl << " = |\t";
-	for (int i = 0; i < n; cout << v[i++] << "\t");
-	cout << "|\n";
+	return ret;
 }
